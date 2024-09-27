bbbfef07d9d92aae4c224acd8d2eda26bd7ad92c
Yevgeniy Valeyev <webmaster.vea@gmail.com>
2024-09-27 16:22:16 +0200

    chore(auth): api protection

---
+++ api/src/auth/auth.js
@@ -0,0 +1,32 @@
+const allowedUsers = ["yevgeniyvaleyev"];
+
+function authorizeRequest(req) {
+  const clientPrincipalHeader = req.headers["x-ms-client-principal"];
+
+  if (!clientPrincipalHeader) {
+    return {
+      status: 401,
+      body: "Unauthorized. Please log in to access this API.",
+    };
+  }
+
+  // Decode the Base64-encoded user info
+  const decodedPrincipal = Buffer.from(
+    clientPrincipalHeader,
+    "base64",
+  ).toString("utf8");
+  const user = JSON.parse(decodedPrincipal);
+
+  if (!allowedUsers.includes(user.userDetails)) {
+    return {
+      status: 403,
+      body: "Access denied. You are not authorized to access this API.",
+    };
+  }
+
+  return null;
+}
+
+module.exports = {
+  authorizeRequest,
+};
--- api/src/functions/items.js
+++ api/src/functions/items.js
@@ -1,13 +1,20 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
+const { authorizeRequest } = require("../auth/auth");
 
-app.http('items', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+app.http("items", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    const unauthorizedResponseObject = authorizeRequest(request);
 
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+    if (unauthorizedResponseObject) {
+      return unauthorizedResponseObject;
     }
+
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get("name") || (await request.text()) || "world";
+
+    return { body: `Hello, ${name}!` };
+  },
 });
