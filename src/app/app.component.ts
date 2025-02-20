244abc2fdb368cc37b1f9cdd9110a80012e5449a
Yevgeniy Valeyev <webmaster.vea@gmail.com>
2025-02-20 10:46:59 +0100

    chore(caching):improved caching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterOutlet } from '@angular/router';
 import { PwaUpdateService } from 'core/services/pwa-update.service';
+import { AuthStore } from './core/store/auth.store';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,
     RouterOutlet,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatSnackBarModule,
+    MatProgressBarModule,
   ],
-  template: '<router-outlet />',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   private pwaUpdateService = inject(PwaUpdateService);
+  private authStore = inject(AuthStore);
+
+  public isLoading = this.authStore.loading;
 
   constructor() {
     this.pwaUpdateService.checkForUpdate();
